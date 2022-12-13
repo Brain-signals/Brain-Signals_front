868f532fd21dcb8406c1eaf07ea39ea618006e42
Adrien Combes <me@adriencombes.com>
2022-12-13 12:44:01 +0100

    changed something

--- utils.py
+++ utils.py
@@ -30,54 +30,54 @@ def show_nii_2D(volume):
     return fig
 
 
-def show_nii_3D(volume):
-    volume = ndimage.zoom(volume,np.array([96/volume.shape[0], 96/volume.shape[1], 1]))
-    # Define frames
-    nb_frames = volume.shape[-1] - 1 #minus 1 frame for initial display
-    fig = go.Figure(frames=[go.Frame(data=go.Surface(z=(nb_frames/10 - k * 0.1) * np.ones((96, 96)),
-                                                    surfacecolor=np.rot90(volume[:,:,nb_frames - k]),
-                                                    cmin=0, cmax=255
-                                                    ),
-                                    name=str(k) # you need to name the frame for the animation to behave properly
-                                    )
-                    for k in range(20,nb_frames-10)])
+# def show_nii_3D(volume):
+#     volume = ndimage.zoom(volume,np.array([96/volume.shape[0], 96/volume.shape[1], 1]))
+#     # Define frames
+#     nb_frames = volume.shape[-1] - 1 #minus 1 frame for initial display
+#     fig = go.Figure(frames=[go.Frame(data=go.Surface(z=(nb_frames/10 - k * 0.1) * np.ones((96, 96)),
+#                                                     surfacecolor=np.rot90(volume[:,:,nb_frames - k]),
+#                                                     cmin=0, cmax=255
+#                                                     ),
+#                                     name=str(k) # you need to name the frame for the animation to behave properly
+#                                     )
+#                     for k in range(20,nb_frames-10)])
 
-    # Add data to be displayed before animation starts
-    fig.add_trace(go.Surface(
-        z=nb_frames/10 * np.ones((96, 96)),
-        surfacecolor=np.rot90(volume[:,:,nb_frames]),
-        colorscale='Gray',
-        cmin=0, cmax=255,
-        colorbar=dict(thickness=20, ticklen=4)
-        ))
+#     # Add data to be displayed before animation starts
+#     fig.add_trace(go.Surface(
+#         z=nb_frames/10 * np.ones((96, 96)),
+#         surfacecolor=np.rot90(volume[:,:,nb_frames]),
+#         colorscale='Gray',
+#         cmin=0, cmax=255,
+#         colorbar=dict(thickness=20, ticklen=4)
+#         ))
 
-    # Layout
-    fig.update_layout(
-        title='Slices in volumetric data',
-        width=600,
-        height=600,
-        scene=dict(
-            zaxis=dict(range=[0, nb_frames/10], autorange=False),
-            aspectratio=dict(x=1, y=1, z=1)),
-        updatemenus = [{"buttons": [
-                      {
-                          "args": [None, frame_args(50)],
-                          "label": "&#9654;", # play symbol
-                          "method": "animate",
-                      },
-                      {
-                          "args": [[None], frame_args(0)],
-                          "label": "&#9724;", # pause symbol
-                          "method": "animate",
-                      },
-                  ],
-                  "direction": "left",
-                  "pad": {"r": 10, "t": 70},
-                  "type": "buttons",
-                  "x": 0.1,
-                  "y": 0,
-              }
-          ],
-          sliders=sliders
-    )
-  return fig
+#     # Layout
+#     fig.update_layout(
+#         title='Slices in volumetric data',
+#         width=600,
+#         height=600,
+#         scene=dict(
+#             zaxis=dict(range=[0, nb_frames/10], autorange=False),
+#             aspectratio=dict(x=1, y=1, z=1)),
+#         updatemenus = [{"buttons": [
+#                       {
+#                           "args": [None, frame_args(50)],
+#                           "label": "&#9654;", # play symbol
+#                           "method": "animate",
+#                       },
+#                       {
+#                           "args": [[None], frame_args(0)],
+#                           "label": "&#9724;", # pause symbol
+#                           "method": "animate",
+#                       },
+#                   ],
+#                   "direction": "left",
+#                   "pad": {"r": 10, "t": 70},
+#                   "type": "buttons",
+#                   "x": 0.1,
+#                   "y": 0,
+#               }
+#           ],
+#           sliders=sliders
+#     )
+#   return fig
